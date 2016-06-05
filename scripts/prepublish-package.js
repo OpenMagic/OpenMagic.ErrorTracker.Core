5c9e134f99017a6cc257612e5206b55315426cb7
Tim Murphy <tim@26tp.com>
2016-06-06 08:47:51 +1000

    Refactor npm script 'prepublish-package.js'

--- scripts/prepublish-package.js
+++ scripts/prepublish-package.js
@@ -8,42 +8,8 @@ const newVersion = getNewVersionArgument();
 
 shell.exec(`npm version ${newVersion}`);
 
-function getArgvAsJson(argvAsString) {
-    try {
-        return JSON.parse(argvAsString);
-    } catch (error) {
-        writeUsage();
-        throw new Error(`Cannot parse process.env.npm_config_argv '${argvAsString}'.\n\n${error}`);
-    }
-}
-
-function getArgvAsString() {
-    const argvAsString = process.env.npm_config_argv;
-
-    if (argvAsString == null) {
-        writeUsage();
-        throw new Error(`process.env.npm_config_argv cannot be null.`);
-    }
-
-    return argvAsString;
-}
-
-function getOriginalCommandLineArguments(argvAsJson) {
-    const originalCommandLineArguments = argvAsJson.original;
-
-    if (originalCommandLineArguments == null) {
-        writeUsage();
-        throw new Error(`Cannot get original command line arguments from process.env.npm_config_argv '${process.env.npm_config_argv}'.`);
-    }
-
-    return originalCommandLineArguments;
-}
-
 function getNewVersionArgument() {
-    const argvAsString = getArgvAsString();
-    const argvAsJson = getArgvAsJson(argvAsString);
-    const originalCommandLineArguments = getOriginalCommandLineArguments(argvAsJson);
-    const newVersion = originalCommandLineArguments.pop();
+    const newVersion = process.argv[process.argv.length - 1];
     const acceptsNewVersions = ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease', 'from-git'];
 
     if (!acceptsNewVersions.includes(newVersion)) {
