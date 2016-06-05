52cdf46c2ab300a8b554a8aa28732412e622eea8
Tim Murphy <tim@26tp.com>
2016-06-06 09:25:44 +1000

    1.0.2

--- source/OpenMagic.ErrorTracker.Core/Constants.cs
+++ source/OpenMagic.ErrorTracker.Core/Constants.cs
@@ -8,7 +8,7 @@
             public const string FileVersion = Version;
             public const string Product = "OpenMagic.ErrorTracker";
             public const string Title = Product;
-            public const string Version = "1.0.1.0";
+            public const string Version = "1.0.2.0";
         }
     }
 }
