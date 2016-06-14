ddc97c13ea268036c52e9841cf6d0af4ec5f88bc
Tim Murphy <tim@26tp.com>
2016-06-15 02:05:28 +1000

    Add specifications project

---
+++ tests/OpenMagic.ErrorTracker.Core.Specifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using Anotar.LibLog;
+using OpenMagic.ErrorTracker.Core;
+
+[assembly: AssemblyTitle(Constants.Assembly.Title + ".Core")]
+[assembly: AssemblyDescription("Specifications for " + Constants.Assembly.Title + ".Core related projects.")]
+[assembly: AssemblyProduct(Constants.Assembly.Product)]
+[assembly: AssemblyCopyright(Constants.Assembly.Copyright)]
+[assembly: AssemblyVersion(Constants.Assembly.Version)]
+[assembly: AssemblyFileVersion(Constants.Assembly.FileVersion)]
+[assembly: LogMinimalMessage]
