f0d3875569b06fa20e87a68fefcb2c066a6d2efb
Tim Murphy <tim@26tp.com>
2016-06-24 09:55:43 +1000

    Replace IEventsQueue with IEventStore

---
+++ source/OpenMagic.ErrorTracker.Core/Repositories/IEventStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenMagic.ErrorTracker.Core.Events;
+
+namespace OpenMagic.ErrorTracker.Core.Repositories
+{
+    /// <summary>
+    ///     Store and read events for an aggregate.
+    /// </summary>
+    public interface IEventStore
+    {
+        /// <summary>
+        ///     Add an <see cref="IEvent">event</see> for the specified <typeparamref name="TAggregate">aggregate</typeparamref>.
+        /// </summary>
+        /// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
+        /// <param name="aggregateId">The aggregate identifier.</param>
+        /// <param name="event">The event to store.</param>
+        Task AddAsync<TAggregate>(Guid aggregateId, IEvent @event);
+
+        /// <summary>
+        ///     Add an <see cref="IEvent">event</see> for the specified <paramref name="aggregateType">aggregate</paramref>.
+        /// </summary>
+        /// <param name="aggregateType">The type of the aggregate.</param>
+        /// <param name="aggregateId">The aggregate identifier.</param>
+        /// <param name="event">The event to store.</param>
+        Task AddAsync(Type aggregateType, Guid aggregateId, IEvent @event);
+    }
+}
